1fa5eb4319e2a8ebda053b7a7de44415f0a360f1
Erik Nordmark <eriknordmark@Eriks-MBP.lan>
2018-11-28 11:48:07 +0100

    Build fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="app">
+            <div className="splash"></div>
+            <div className="logo"></div>
+            <section>
+                <h2>Vinmässa 2019</h2>
+                <p>9/2 Quality Hotel, Vänersborg. 16.00 - 22.00</p>
+                <a href="mailto:info@bordeauxsvanner.se">info@bordeauxsvanner.se</a>
+            </section>
+            <aside>
+                <h3>Veckans vintips</h3>
+                <h4>Côtes-du-Rhône 2015 E. Guigal (2011)</h4>
+            </aside>
+        </div>
     );
-  }
+}
 }
 
 export default App;
